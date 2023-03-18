0153163b1d486c8be072abfe00d767475b8ee926
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-03-18 15:20:24 +0200

    add reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,44 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
+const KEY = '115673062d9a805a3df250beb0ca2927';
 
 export const Reviews = () => {
+    const { moviesId } = useParams();
+    const [review, setReview] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            return await fetch(`https://api.themoviedb.org/3/movie/${moviesId}/reviews?api_key=${KEY}`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                setReview(data.results);
+            })
+              .catch(erorr => alert(erorr));
+          }
+          fetchData();
+    }, [moviesId]);
+
     return (
-        <div>Reviews</div>
+        <div>
+            { review.length !== 0 &&
+            <ul>
+                {
+                  review.map(item => {
+                    return(
+                        <li key={item.id}>
+                       <h3>{item.author}</h3>
+                    <p>{item.content}</p>
+                        </li>
+                    )
+                  })  
+                }
+            </ul>}
+            {
+                review.length === 0 && 
+                <p>We don`t have any reviews for this movie.</p>
+            }
+        </div>
     )
 }
